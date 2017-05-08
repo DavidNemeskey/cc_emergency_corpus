63ef428bb26bdeae9be37b91d5fb9a024576bff1
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2017-05-08 18:00:25 +0200

    Output directory wired into the code.

--- src/main/java/hu/mta/sztaki/hlt/parse_cc/ParseCC.java
+++ src/main/java/hu/mta/sztaki/hlt/parse_cc/ParseCC.java
@@ -1,6 +1,12 @@
 package hu.mta.sztaki.hlt.parse_cc;
 
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import net.sourceforge.argparse4j.ArgumentParsers;
 import net.sourceforge.argparse4j.inf.ArgumentParser;
@@ -12,6 +18,9 @@ import hu.mta.sztaki.hlt.parse_cc.extractors.BoilerpipeExtractor;
 import hu.mta.sztaki.hlt.parse_cc.extractors.JusTextExtractor;
 
 public class ParseCC {
+    /** WARC file name pattern. */
+    private static Pattern warcP = Pattern.compile("^(.+)[.]warc(?:[.]gz)?$");
+
     /** Parses the command line arguments with Commons CLI. */
     public static Namespace parseArguments(String[] args) {
         ArgumentParser parser = ArgumentParsers.newArgumentParser("ParseCC")
@@ -29,7 +38,7 @@ public class ParseCC {
                 .nargs("*")
                 .help("an input WARC file. Note that it must have the .warc " +
                       "or .warc.gz extension.");
-        try{
+        try {
             return parser.parseArgs(args);
         } catch (ArgumentParserException ape) {
             parser.handleError(ape);
@@ -47,13 +56,35 @@ public class ParseCC {
         }
     }
 
+    /**
+     * Returns the output file that corresponds to the specified input WARC.
+     *
+     * @throws IllegalArgumentException if the input file name does not end
+     *                                  with .warc(.gz).
+     */
+    private static String getOutputFile(
+            String outputDirectory, String inputFile)
+            throws IllegalArgumentException {
+        FileSystem fs = FileSystems.getDefault();
+        String baseName = fs.getPath(inputFile).getFileName().toString();
+        Matcher m = warcP.matcher(baseName);
+        if (!m.matches()) {
+            throw new IllegalArgumentException(
+                    String.format("Not a valid input file name: %s; " +
+                                  ".warc(.gz) extension missing.", baseName));
+        }
+        return fs.getPath(outputDirectory, m.group(1) + ".xml").toString();
+    }
+
     public static void main(String[] args) {
         Namespace ns = parseArguments(args);
         Extractor extractor = getExtractor(ns.getString("extractor"));
         for (String inputFile : ns.<String>getList("input_file")) {
             try {
+                String outputFile = getOutputFile(
+                        ns.getString("output_dir"), inputFile);
                 WARCIterator wi = new WARCIterator(inputFile, extractor);
-                XMLConverter converter = new XMLConverter("ide");
+                XMLConverter converter = new XMLConverter(outputFile);
                 for (WARCDocument doc : wi) {
                     converter.convert(doc);
                 }
