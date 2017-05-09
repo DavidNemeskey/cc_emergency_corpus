81129425e6101b0650d22319b4f6b56ff45e3f79
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2017-05-09 11:57:21 +0200

    File logging.

--- src/main/java/hu/mta/sztaki/hlt/parse_cc/ParseCC.java
+++ src/main/java/hu/mta/sztaki/hlt/parse_cc/ParseCC.java
@@ -40,9 +40,13 @@ public class ParseCC {
                 .choices("boilerpipe", "justext")
                 .setDefault("boilerpipe")
                 .help("the text extractor to use.");
-        parser.addArgument("-l", "--log-level")
+        parser.addArgument("-l", "--log-dir")
+                .help("the log directory. If specified, a separate log file " +
+                      "is created for each input file; otherwise, logs are " +
+                      "written to stderr.");
+        parser.addArgument("-L", "--log-level")
                 .choices(getLevels())
-                .setDefault("INFO")
+                .setDefault("OFF")
                 .help("the logging level.");
         parser.addArgument("input_file")
                 .nargs("*")
@@ -69,11 +73,15 @@ public class ParseCC {
     /**
      * Returns the output file that corresponds to the specified input WARC.
      *
+     * @param outputDirectory the output directory.
+     * @param inputFile the name of the original file.
+     * @param extension the extension of the output file that replaces the
+     *                  .warc(.gz) extension of the original.
      * @throws IllegalArgumentException if the input file name does not end
      *                                  with .warc(.gz).
      */
     private static String getOutputFile(
-            String outputDirectory, String inputFile)
+            String outputDirectory, String inputFile, String extension)
             throws IllegalArgumentException {
         FileSystem fs = FileSystems.getDefault();
         String baseName = fs.getPath(inputFile).getFileName().toString();
@@ -83,17 +91,23 @@ public class ParseCC {
                     String.format("Not a valid input file name: %s; " +
                                   ".warc(.gz) extension missing.", baseName));
         }
-        return fs.getPath(outputDirectory, m.group(1) + ".xml").toString();
+        return fs.getPath(
+                outputDirectory, m.group(1) + "." + extension).toString();
     }
 
     public static void main(String[] args) {
         Namespace ns = parseArguments(args);
-        logger = configureLogging(Level.parse(ns.getString("log_level")));
         Extractor extractor = getExtractor(ns.getString("extractor"));
+        String logDir = ns.getString("log_dir");
+        logger = configureLogging(Level.parse(ns.getString("log_level")));
         for (String inputFile : ns.<String>getList("input_file")) {
             try {
                 String outputFile = getOutputFile(
-                        ns.getString("output_dir"), inputFile);
+                        ns.getString("output_dir"), inputFile, "xml");
+                if (logDir != null) {
+                    logger = configureLogging(Level.parse(ns.getString("log_level")),
+                                              getOutputFile(logDir, inputFile, "log"));
+                }
                 WARCIterator wi = new WARCIterator(inputFile, extractor);
                 XMLConverter converter = new XMLConverter(outputFile);
                 logger.info(String.format("Converting %s to %s...",
