5f53889dea22c9848b47fda5816f57cc57e4ea7b
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2017-05-11 07:16:46 +0200

    E

--- python/scripts/filter_language.py
+++ python/scripts/filter_language.py
@@ -5,9 +5,12 @@
 
 from __future__ import absolute_import, division, print_function
 import argparse
+import os
+from queue import Empty
 
-for doc in parse_xml(input_stream):
-    pass  # 1st is header
+from cc_emergency.utils import openall
+from cc_emergency.utils import run_queued, setup_queue_logger
+from cc_emergency.xml import parse_xml
 
 
 def parse_arguments():
@@ -21,22 +24,55 @@ def parse_arguments():
                         help='the number of files to process parallelly.')
     args = parser.parse_args()
 
-    # Config file
-    pp = partial(config_pp, args=args)
-    config, warnings, errors = load_config(
-        args.configuration, 'lstm_lm_conf.schema', pp)
-    handle_errors(warnings, errors)
+    return args
 
-    if not args.train and not args.test:
-        parser.error('At least one of the train or test sets must be specified.')
-    if not args.train and args.reset == 1:
-        parser.error('The reset option only works for training.')
 
-    return args, config
+def process_file(language, queue, logging_level=None, logging_queue=None):
+    logger = setup_queue_logger(logging_level, logging_queue)
+    try:
+        while True:
+            try:
+                infile, outfile = queue.get_nowait()
+                logger.info('Started processing {}'.format(infile))
+                with openall(infile, 'rb') as inf:
+                    doc_it = parse_xml(inf, True)
+                    header = next(doc_it)
+                    for doc in doc_it:
+                        print(doc['url'])
+                logger.info('Done processing {}'.format(infile))
+            except Empty:
+                logger.debug('Queue depleted.')
+                break
+            except:
+                logger.exception('Exception in file {}'.format(
+                    infile))
+    except Exception as e:
+        logger.exception('Unexpected exception')
+        raise
+
+
+def source_target_file_list(source_dir, target_dir):
+    source_dir = op.abspath(source_dir)
+    target_dir = op.abspath(target_dir)
+    source_files = [op.abspath(op.join(d, f))
+                    for d, _, fs in walk_non_hidden(source_dir) for f in fs]
+    target_files = []
+    for sf in source_files:
+        sf_rel = sf[len(source_dir):].lstrip(os.sep)
+        tf = op.join(target_dir, sf_rel)
+        td = op.dirname(tf)
+        if not op.isdir(td):
+            os.makedirs(td)
+        target_files.append(tf)
+    return zip(source_files, target_files)
 
 
 def main():
     args = parse_arguments()
+    os.nice(20)  # Play nice
+
+    source_target_files = source_target_file_list(args.source_dir, args.target_dir)
+    run_queued(process_file, 'en', args.processes, source_target_files, args.log_level)
 
 
 if __name__ == '__main__':
