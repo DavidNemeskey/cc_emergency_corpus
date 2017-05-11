9f3a9235b624c4d66fe24277e4dced3d33d21205
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2017-05-11 17:10:59 +0200

    Added the functional module.

---
+++ python/cc_emergency/functional/core.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8 :
+
+"""
+Defines the streams classes that can be used to implement functional data
+transformation schemes, such as map-reduce.
+
+The names of the classes are influenced by the Java Stream framework, but this
+is only "skin-deep"; the user should not expect that the classes here work in
+the same way as their Java not-counterparts.
+
+TODO: check out PyFunctional. It seems to have implemented this already, but
+      it seems to store everything in memory, which might be suboptimal with
+      huge datasets.
+"""
+
+
+class Stream(object):
+    """
+    An iterator that defines functions map and filter as convenience.
+
+    NOTE: do we need this?
+    """
+    def __init__(self, it):
+        self.it = it
+
+    def map(self, transform):
+        return map(transform, self.it)
+
+    def filter(self, transform):
+        return filter(transform, self.it)
+
+
+class Resource(object):
+    """
+    The base class for the processing steps in the pipeline. Provides functions
+    to initialize and clean up the underlying resources.
+    """
+    def initialize(self):
+        """
+        Initializes any resources the object might use. This should be
+        done here so that it is only run once in a multiprocessing setting.
+        """
+        pass
+
+    def cleanup(self):
+        """The opposite of initialize()."""
+        pass
+
+
+class Source(Resource):
+    """
+    Produces an iterable. Each transformation pipeline starts with a Source.
+    """
+    def __iter__(self, it):
+        raise NotImplementedError(
+            "__iter__() is not implemented in " + self.__class__.__name__)
+
+
+class Transform(Resource):
+    """
+    A single transformation. map() and filter() call this function with a
+    different purpose.
+    """
+    def __call__(self, param):
+        raise NotImplementedError(
+            "__call__() is not implemented in " + self.__class__.__name__)
+
+
+class Collector(Resource):
+    """
+    Collects the iterable presented to it. This is a terminal operation, as it
+    returns a single value, if any.
+    """
+    def collect(self, it):
+        raise NotImplementedError(
+            "collect() is not implemented in " + self.__class__.__name__)
---
+++ python/cc_emergency/functional/io.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8 :
+
+"""IO classes."""
+
+from __future__ import absolute_import, division, print_function
+from collections import OrderedDict
+import json
+
+from cc_emergency.utils import openall
+from cc_emergency.functional.core import Collector, Resource, Source
+
+
+class FileWrapper(Resource):
+    """Wraps a file stream."""
+    def __init__(self, file, mode='rt', *args, **kwargs):
+        super(FileWrapper, self).__init__(*args, **kwargs)
+        self.file = file
+        self.mode = mode
+
+    def initialize(self):
+        """Opens the file."""
+        self.stream = openall(self.file, mode=self.mode)
+
+    def cleanup(self):
+        """Closes the input stream."""
+        self.stream.close()
+
+
+class JsonReader(Source, FileWrapper):
+    """Reads a text file that has a JSON object on each line."""
+    def __init__(self, input_file, ordered=True):
+        """
+        Parameters:
+        - input_file the input file
+        - ordered whether the order of keys in a dictionary should be kept or
+                  not; the default is True.
+        """
+        super(JsonReader, self).__init__(input_file)
+        if ordered:
+            self.decoder = json.JSONDecoder(object_pairs_hook=OrderedDict)
+        else:
+            self.decoder = json.JSONDecoder()
+
+    def __iter__(self):
+        for line in self.stream:
+            yield json.loads(line)
+
+
+class JsonWriter(Collector, FileWrapper):
+    """Writes a text file that has a JSON object on each line."""
+    def __init__(self, output_file):
+        super(JsonWriter, self).__init__(output_file, 'wt')
+
+    def collect(self, it):
+        for obj in it:
+            print(json.dumps(obj), file=self.stream)
