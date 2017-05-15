ecbd0a1576599d59cc7c760cd9fa16e2c5efa6e6
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2017-05-15 17:07:33 +0200

    Moved around stuff in transform/.

--- python/cc_emergency/functional/transforms/corenlp.py
+++ python/cc_emergency/functional/transforms/corenlp.py
@@ -1,133 +1,66 @@
 #!/usr/bin/env python3
-"""Manages a Stanford CoreNLP server."""
+"""A preprocessor that invokes a Stanford CoreNLP server for analysis."""
 
 from __future__ import absolute_import, division, print_function
-try:
-    from configparser import RawConfigParser
-except:
-    from ConfigParser import RawConfigParser
 import logging
-# from future.moves.urllib.parse import urlencode
-import os
-from subprocess import Popen
-import time
 
-import requests
+from cc_emergency.functional.core import Transform
+from cc_emergency.functional.transforms.corenlp_backend import CoreNLPBackend
 
 
-class CoreNLPError(Exception):
-    pass
-
-
-class CoreNLP(object):
-    """Stanford CoreNLP interface object."""
-    def __init__(self, corenlp_props):
+class CoreNLP(Transform):
+    """A preprocessor that invokes a Stanford CoreNLP server for analysis."""
+    def __init__(self, props, fields, max_length=10000):
         """
-        corenlp_props is a dictionary that contains the properties for
-        the CoreNLP server under the [URL] section. There must also be three keys
-        under the [server] section:
-          - directory, that specifies the CoreNLP installation path;
-          - port, which is the port the CoreNLP server is on localhost;
-          - memory, for the java process (the default is 4g).
+        Parameters:
+        - corenlp_props the properties file for the server (see CoreNLP).
+        - fields the fields to parse. Each yields a new field called
+                 field + '_corenlp'
+        - max_length: the maximum chunk size.
         """
+        self.corenlp_props = props
+        self.fields = fields
+        self.max_length = max_length
+        self.corenlp = None
         self.logger = logging.getLogger(
             self.__class__.__module__ + '.' + self.__class__.__name__)
-        self.directory = corenlp_props['server']['directory']
-        self.memory = corenlp_props['server'].get('memory', '4g')
-        self.port = corenlp_props['server'].get('port', 9000)
-        self.url = 'http://localhost:{}'.format(self.port)
-        self.props = str(corenlp_props['URL'])
-        self.server = None
-        self.shutdown_key = None
-        self.parsed = 0
-        self.__start_server()
 
-    def __del__(self):
-        # See also http://stackoverflow.com/questions/865115/how-do-i-correctly-clean-up-a-python-object
-        # for ideas on how to replace __del__ with something better (?)
-        self.__stop_server()
+    def __enter__(self):
+        """
+        The CoreNLP server is initialized here so that it is only created once,
+        in the processing process, not in the main one.
+        """
+        if not self.corenlp:
+            self.corenlp = CoreNLPBackend(self.corenlp_props)
 
-    def __start_server(self):
-        self.logger.debug("Starting server {}...".format(self.url))
-        # TODO eat the server's output -- in this case, there is no need to wait
-        self.server = Popen(
-            ['java', '-mx{}'.format(self.memory),
-             '-cp', '*', 'edu.stanford.nlp.pipeline.StanfordCoreNLPServer',
-             '--port', self.port, '--server_id', self.port, '--quiet'],
-            cwd=self.directory
-        )
-        self.parsed = 0
-        time.sleep(5)
-        with open('/tmp/corenlp.shutdown.{}'.format(self.port)) as inf:
-            self.shutdown_key = inf.read()
-        self.logger.info("Started server {}".format(self.url))
+    def __exit__(self, *args):
+        if self.corenlp:
+            del self.corenlp
+        self.corenlp = None
 
-    def __stop_server(self):
-        self.logger.debug("Stopping server? {}...".format(self.url))
-        if self.server:
-            self.logger.debug("Stopping server {}...".format(self.url))
-            try:
-                requests.post('{}/shutdown?key={}'.format(
-                    self.url, self.shutdown_key))
-            except:
-                raise
-            self.server.communicate()
-            self.logger.info("Stopped server {}".format(self.url))
-        self.server = None
-
-    def __restart_server(self):
-        self.logger.debug("Restarting server {}...".format(self.url))
-        self.__stop_server()
-        self.__start_server()
-
-    def parse(self, text, anas=False):
-        """Parses a text with a running CoreNLP server."""
-        if not self.server:
-            self.__start_server()
-        with open('/dev/shm/text-{}'.format(os.getpid()), 'wt') as outf:
-            print(text.encode('utf-8'), file=outf)
-
+    def __call__(self, obj):
         try:
-            reply = self.__send_request(text)
-            if reply:
-                return self.__split_result(reply.decode('utf-8'))
-        except CoreNLPError as cnlpe:
-            self.__stop_server()
-            raise
-        except:
-            self.__stop_server()
-            raise
+            for field in self.fields:
+                if field in obj and obj[field]:
+                    obj[field + '_corenlp'] = []
+                    for parsed in self.__parse_with_corenlp(obj[field]):
+                        obj[field + '_corenlp'].extend(parsed)
+        except Exception as e:
+            self.logger.exception('Error in document {}'.format(obj['url']))
+        return obj
 
-    def __send_request(self, text):
-        for tries in range(3):
-            try:
-                r = requests.post(self.url, params={'properties': self.props},
-                                  data=text.encode('utf-8'))
-                if r.status_code != 200:
-                    self.logger.error(
-                        u'Server {} returned an illegal status code {}'.format(
-                            self.url, r.status_code))
-                    r = None
-            except Exception as e:
-                self.logger.exception(
-                    u'Exception {} while trying to access server {}'.format(
-                        e, self.url))
-                r = None
-            if not r:
-                self.__restart_server()
-            if r:
-                return r.content
-        else:
-            raise CoreNLPError(u'Number of tries exceeded with url {}'.format(self.url))
-
-    @staticmethod
-    def __split_result(text):
+    def __parse_with_corenlp(self, text):
         """
-        Splits the result string into lists of lists. Also deletes the
-        per-sentence token index (the very first column) from the
-        conll-format reply returned by the server.
+        Parses the input with CoreNLP. This generator is called from
+        __call__(). Reads from the text a batch of lines, and
+        yields the parsed data chunk.
         """
-        return [
-            [token.split('\t')[1:] for token in sentence.split('\n')]
-            for sentence in text.split('\n\n') if sentence != ''
-        ]
+        chunk, chunk_len = [], 0
+        for txt in text.split('\n'):
+            chunk.append(txt)
+            chunk_len += len(txt)
+            if chunk_len > self.max_length:
+                yield self.corenlp.parse('\n'.join(chunk))
+                chunk, chunk_len = [], 0
+        if chunk:
+            yield self.corenlp.parse('\n'.join(chunk))
