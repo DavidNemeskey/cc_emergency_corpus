e42064b588ff494109a7ba13b762bebdc5892775
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2017-05-17 11:57:32 +0200

    tf_df_counter.py completed.

--- python/cc_emergency/functional/collectors/tf_df_counter.py
+++ python/cc_emergency/functional/collectors/tf_df_counter.py
@@ -5,28 +5,28 @@
 Counts the occurrences of tokens in the fields specified by the user, and
 aggregates them both as (corpus) TF and DF.
 """
+
 from future.utils import viewkeys
 from collections import Counter
 
+from cc_emergency.functional.core import Collector
+
 
 class TFDFCollector(Collector):
-    def __init__(self, field_weights, tf_df_names=None):
+    def __init__(self, field_weights):
         """
         The field_weights dictionary: {field: weight} specifies which fields to
         convert, and how much weight to give the tokens therein. This only
         affects the TF computation.
-
-        tf_df_names is a tuple that sets the name of the TF and DF fields. The
-        default, not surprisingly, is ("TF", "DF").
         """
         self.field_weights = field_weights
-        self.tf_df_names = ("TF", "DF") if not tf_df_names else tf_df_names
-        self.logger = logging.getLogger(
-            self.__class__.__module__ + '.' + self.__class__.__name__)
 
     def __call__(self, it):
-        df, tf = Counter(), Counter()
+        tf, df = Counter(), Counter()
         for obj in it:
-        dfs = set()
-        for field, weight in self.field_weights.items():
-            dfs |= obj[field
+            dfs = set()
+            for field, weight in self.field_weights.items():
+                dfs |= viewkeys(obj[field])
+                tf.update({w: f * weight for w, f in obj[field].items()})
+            df.update(dfs)
+        return tf, df
