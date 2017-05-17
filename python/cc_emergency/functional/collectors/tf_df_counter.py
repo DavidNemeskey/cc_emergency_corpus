7f135bd48c99115331f2d5f04c552afb7d0ff565
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2017-05-17 12:07:33 +0200

    Back to named fields.

--- python/cc_emergency/functional/collectors/tf_df_counter.py
+++ python/cc_emergency/functional/collectors/tf_df_counter.py
@@ -13,13 +13,17 @@ from cc_emergency.functional.core import Collector
 
 
 class TFDFCollector(Collector):
-    def __init__(self, field_weights):
+    def __init__(self, field_weights, tf_df_names=None):
         """
         The field_weights dictionary: {field: weight} specifies which fields to
         convert, and how much weight to give the tokens therein. This only
         affects the TF computation.
+
+        tf_df_names is a tuple that sets the name of the TF and DF fields. The
+        default, not surprisingly, is ("TF", "DF").
         """
         self.field_weights = field_weights
+        self.tf_df_names = ("TF", "DF") if not tf_df_names else tf_df_names
 
     def __call__(self, it):
         tf, df = Counter(), Counter()
@@ -29,4 +33,4 @@ class TFDFCollector(Collector):
                 dfs |= viewkeys(obj[field])
                 tf.update({w: f * weight for w, f in obj[field].items()})
             df.update(dfs)
-        return tf, df
+        return {self.tf_df_names[0]: tf, self.tf_df_names[1]: df}
