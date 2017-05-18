15cc48aa3a1cf4c400b26a11ea69cb8abf51f9f7
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2017-05-18 11:23:48 +0200

    Fixed wrong super() order.

--- python/cc_emergency/functional/transforms/dict_transforms.py
+++ python/cc_emergency/functional/transforms/dict_transforms.py
@@ -10,7 +10,7 @@ from cc_emergency.functional.core import Filter, Map
 class DeleteFields(Map):
     """Deletes fields from a document."""
     def __init__(self, fields):
-        super(self, DeleteFields).__init__()
+        super(DeleteFields, self).__init__()
         self.fields = fields
 
     def transform(self, obj):
@@ -20,7 +20,7 @@ class DeleteFields(Map):
 class RetainFields(Map):
     """Retains selected fields of a document, dropping all the rest."""
     def __init__(self, fields):
-        super(self, RetainFields).__init__()
+        super(RetainFields, self).__init__()
         self.fields = fields
 
     def transform(self, obj):
@@ -33,7 +33,7 @@ class FilterEmpty(Filter):
     record is dropped.
     """
     def __init__(self, fields):
-        super(self, FilterEmpty).__init__()
+        super(FilterEmpty, self).__init__()
         self.fields = fields
 
     def transform(self, obj):
