bd452d8920dd59c8129b336879a272c3ba7db324
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2017-05-26 15:42:46 +0200

    Replacing NNPs with a user specified token. Fixes #19.

--- python/cc_emergency/functional/transforms/conll_converter.py
+++ python/cc_emergency/functional/transforms/conll_converter.py
@@ -18,14 +18,18 @@ class ConvertCoNLL(Map):
 
     TODO: convert to an ABC.
     """
+    WORD, LEMMA, POS = range(3)
+
     def __init__(self, fields_columns):
         """
         The fields_columns dictionary:
-        {field: [column, lower, delete, new_field]}
+        {field: [column, lower, convert_nnps, delete, new_field]}
         specifies which fields to convert, and then which column to use as the
         token. The column can be a number, or the words "word" (0) and
         "lemma" (1). lower indicates whether the token should be lowercased,
         while new_field is the name of the field where the result is put.
+
+        If convert_nnps is specified, all NNP tokens are replaced by its value.
         """
         super(ConvertCoNLL, self).__init__()
         self.fields_columns = {
@@ -35,11 +39,13 @@ class ConvertCoNLL(Map):
 
     def transform(self, obj):
         for field, spec in self.fields_columns.items():
-            column, lower, delete, new_field = spec
+            column, lower, convert_nnps, delete, new_field = spec
             if field in obj:
-                tokens = [[token[column].lower() if lower else token[column]
-                          for token in sentence] for sentence in obj[field]]
-            obj[new_field] = self.format(tokens)
+                tokens = [[self.__convert(token, column, lower, convert_nnps)
+                           for token in sentence] for sentence in obj[field]]
+                obj[new_field] = self.format(tokens)
+                if delete:
+                    del obj[field]
         return obj
 
     def format(self, tokens):
@@ -47,14 +53,22 @@ class ConvertCoNLL(Map):
         raise NotImplementedError(
             'format is not implemented; use one of the subclasses.')
 
+    def __convert(self, token, column, lower, convert_nnps):
+        ret = token[column]
+        if lower:
+            ret = ret.lower()
+        if convert_nnps and token[ConvertCoNLL.POS].startswith('NNP'):
+            ret = convert_nnps
+        return ret
+
     @staticmethod
     def __column(column):
         if isinstance(column, int):
             return column
         elif column.lower() == "word":
-            return 0
+            return ConvertCoNLL.WORD
         elif column.lower() == "lemma":
-            return 1
+            return ConvertCoNLL.LEMMA
         else:
             raise ValueError('Invalid column specifier "{}"'.format(column))
 
