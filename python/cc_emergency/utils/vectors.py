dda6e2f44755ba935d07d90b64a7b3fc68c67b75
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2017-05-30 10:37:11 +0200

    Works?

--- python/cc_emergency/utils/vectors.py
+++ python/cc_emergency/utils/vectors.py
@@ -69,9 +69,26 @@ def write_vectors(words, vectors, vectors_file):
 
 
 def compute_mds(vectors, dist_type, processes=1):
-    mds = manifold.MDS(metric=True, n_jobs=processes)
+    """
+    Uses metric multidimensional scaling with angular distance to map the
+    points in vectors to two dimensions.
+    """
+    if dist_type.lower().startswith('euc'):
+        mds = manifold.MDS(metric=True, n_jobs=processes)
+        X = vectors
+    elif dist_type.lower().startswith('cos'):
+        mds = manifold.MDS(metric=True, n_jobs=processes,
+                           dissimilarity='precomputed')
+        X = __angular_distance(vectors)
+    else:
+        raise ValueError('dist_type must be euc or cos')
+
+    results = mds.fit(X)
+    print('Results: {}'.format(results))
+    return results.embedding_
 
 
-def __compute_cos(vectors):
-    """Computes the cosine similarities for the vectors."""
-
+def __angular_distance(vectors):
+    """Computes the angular distances between the vectors."""
+    cosine_similarity = vectors.dot(vectors.T)
+    return np.arccos(cosine_similarity) / np.pi
--- python/scripts/display_bev.py
+++ python/scripts/display_bev.py
@@ -6,12 +6,17 @@
 from __future__ import absolute_import, division, print_function
 import argparse
 
+import matplotlib.pyplot as plt
+
 from cc_emergency.utils.vectors import read_vectors, write_vectors
+from cc_emergency.utils.vectors import compute_mds
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser(
-        description='Displays the BEV in 2D.')
+        description='Displays the BEV in 2D. Uses metric multidimensional '
+                    'scaling with angular distance to map the points to '
+                    'two dimensions.')
     parser.add_argument('vector_file', help='the word vector file.')
     parser.add_argument('--bev', '-b',
                         help='the BEV list file (one word per line). If not '
@@ -30,21 +35,32 @@ def parse_arguments():
     return args
 
 
-def vector_stuff(vector_file, bev_file, write_vectors):
+def vector_stuff(vector_file, bev_file, write_file):
     if bev_file:
         with open(bev_file) as inf:
             bev = set(inf.read().split('\n'))
     else:
         bev = None
     words, vectors = read_vectors(vector_file, bev)
-    if write_vectors:
-        write_vectors(words, vectors, write_vectors)
+    if write_file:
+        write_vectors(words, vectors, write_file)
     return words, vectors
 
 
 def main():
     args = parse_arguments()
     words, vectors = vector_stuff(args.vector_file, args.bev, args.write_vectors)
+    coords = compute_mds(vectors, args.distance)
+
+    # Plotting -- ungh...
+    x_min = coords[:, 0].min()
+    x_max = coords[:, 0].max()
+    y_min = coords[:, 1].min()
+    y_max = coords[:, 1].max()
+
+    plt.plot(coords[:, 0], coords[:, 1], 'bx')
+    plt.axis([x_min - 0.1, x_max + 0.1, y_min - 0.1, y_max + 0.1])
+    plt.show()
 
 
 if __name__ == '__main__':
