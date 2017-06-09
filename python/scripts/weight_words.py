67e03167adc610ed2e661e2f8ec2dc4d833e25da
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2017-06-09 16:40:58 +0200

    --print-dfs option (+subcommands).

--- python/scripts/weight_words.py
+++ python/scripts/weight_words.py
@@ -24,41 +24,42 @@ def parse_arguments():
                     'by specifying a second frequency file.')
     parser.add_argument('freq_file',
                         help='the file resulting from reduce_tf_df.')
-    parser.add_argument('base_freq_file', nargs='?',
-                        help='base frequencies for log ratio weighting; a '
-                             'file resulting from reduce_tf_df.')
-    parser.add_argument('--num-docs', '-n', type=int,
-                        help='the total number of documents. Only interesting '
-                             'for the DF case.')
     parser.add_argument('--threshold', '-t', type=float, default=float('-inf'),
                         help='a threshold; words with weights below this are '
                              'dropped.')
     parser.add_argument('--word-filter', '-w',
                         help='a word list file to filter the words we output.')
-    args = parser.parse_args()
+    subparsers = parser.add_subparsers(
+        dest='command', description='',
+        help='choose between DF (IDF computation) and TF (log TF ratio) '
+             'modes of operation.')
+    df_parser = subparsers.add_parser('df', description='Computes the IDFs.')
+    df_parser.add_argument('--num-docs', '-n', type=int,
+                           help='the total number of documents. Only interesting '
+                                'for the DF case.')
+    tf_parser = subparsers.add_parser(
+        'tf', description='Computes the TF log ratios.')
+    tf_parser.add_argument('base_freq_file', nargs='?',
+                           help='base frequencies for log ratio weighting; a '
+                                'file resulting from reduce_tf_df.')
+    tf_parser.add_argument('--print-dfs', '-p', action='store_true',
+                           help='also print the DFs next to the log ratios.')
+    return parser.parse_args()
 
-    if args.num_docs and args.base_freq_file:
-        parser.error('--num-docs is only valid when the base frequency file '
-                     'is not specified.')
-    if not args.num_docs and not args.base_freq_file:
-        parser.error('--num-docs is required if computing DF.')
-    return args
 
-
-def load_file(freq_file, what='DF'):
+def load_file(freq_file):
     with openall(freq_file) as inf:
-        j = json.load(inf)
-    return j[what]
+        return json.load(inf)
 
 
 def compute_idfs(dfs, num_docs):
-    return {word: max(math.log((num_docs - df + 0.5) / (df + 0.5)), 0)
+    return {word: [max(math.log((num_docs - df + 0.5) / (df + 0.5)), 0)]
             for word, df in dfs.items()}
 
 
 def compute_tf_ratio(tfs, base_tfs, size, base_size):
     log_size_ratio = math.log(size / base_size)
-    return {word: math.log(tf / base_tfs[word]) - log_size_ratio
+    return {word: [math.log(tf / base_tfs[word]) - log_size_ratio]
             for word, tf in tfs.items() if base_tfs.get(word)}
 
 
@@ -76,22 +77,28 @@ class WordFilter(object):
 
 def main():
     args = parse_arguments()
-    df_mode = not bool(args.base_freq_file)
     word_filter = WordFilter(args.word_filter)
-    if df_mode:
-        dfs = {w: f for w, f in load_file(args.freq_file, 'DF').items()
-               if word_filter(w)}
+    freqs = load_file(args.freq_file)
+    if args.command == 'df':
+        dfs = {w: f for w, f in freqs['DF'].items() if word_filter(w)}
         to_print = compute_idfs(dfs, args.num_docs)
     else:
-        tfs = {w: f for w, f in load_file(args.freq_file, 'TF').items()
-               if word_filter(w)}
+        tfs = {w: f for w, f in freqs['TF'].items() if word_filter(w)}
         size = sum(tfs.values())
-        base_tfs = load_file(args.base_freq_file, 'TF')
+        base_freqs = load_file(args.base_freq_file)
+        base_tfs = base_freqs['TF']
         base_size = sum(base_tfs.values())
         to_print = compute_tf_ratio(tfs, base_tfs, size, base_size)
-    for word, weight in sorted(to_print.items(), key=lambda kv: (-kv[1], kv[0])):
+        if args.print_dfs:
+            for w, p in to_print:
+                p.append(freqs['DF'][w])
+                p.append(base_freqs['DF'][w])
+
+    for word, payload in sorted(to_print.items(),
+                                key=lambda kv: (-kv[1], kv[0])):
+        weight, *the_rest = payload
         if weight >= args.threshold:
-            print('{}\t{}'.format(word, weight))
+            print('{}\t{}'.format(word, '\t'.join(map(str, payload))))
 
 
 if __name__ == '__main__':
