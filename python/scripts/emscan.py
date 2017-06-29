32b993ba0689f5589b94043f6b1d46876721252a
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2017-06-29 16:35:03 +0200

    Added emscan.py.

---
+++ python/scripts/emscan.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8 :
+
+"""Runs the variants of the emscan algorithm."""
+
+import argparse
+from functools import partial
+import inspect
+import logging
+
+from cc_emergency.utils.vector_io import read_vectors
+from cc_emergency.utils.vectors import emscan_first, emscan_dcg  # noqa
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description='Runs the variants of the emscan algorithm.')
+    parser.add_argument('vector_file', help='the word vector file.')
+    parser.add_argument('query_file', help='the query file, one word per line.')
+    parser.add_argument('--min-similarity', '-s', type=float, default=0.5,
+                        help='the minimum similarity with a cluster node to '
+                             'add a vertex to the candidates.')
+    parser.add_argument('--log-level', '-L', type=str, default='critical',
+                        choices=['debug', 'info', 'warning', 'error', 'critical'],
+                        help='the logging level.')
+    subparsers = parser.add_subparsers(
+        dest='emscan', description='', help='choose the EMSCAN variant.')
+    subparsers.add_parser('first', description='EMSCAN_first')
+    dcg_p = subparsers.add_parser('dcg', description='EMSCAN_dcg')
+    dcg_p.add_argument('--dcg-length', '-l', type=int, default=5,
+                       help='the number of top results to compute the DCG from.')
+    dcg_p.add_argument('--min-dcg', '-m', type=float, default=1,
+                       help='the DCG threshold to add a candidate to the cluster.')
+
+    args = parser.parse_args()
+    if not args.emscan:
+        parser.error('No subcommand specified.')
+    return args
+
+
+def get_emscan_params(args, *fn_args):
+    emscan = globals()['emscan_' + args.emscan]
+    s = inspect.signature(emscan)
+    ba = s.bind_partial(*fn_args)
+    for k in s.parameters.keys():
+        if k not in ba.arguments:
+            ba.arguments[k] = getattr(args, k)
+    return partial(emscan, **ba.arguments)
+
+
+def main():
+    args = parse_arguments()
+    logging.basicConfig(level=getattr(logging, args.log_level.upper()),
+                        format='%(asctime)s - %(levelname)s - %(message)s')
+
+    words, vectors = read_vectors(args.vector_file, normalize=True)
+    with open(args.query_file) as inf:
+        query = [l.strip() for l in inf]
+    qindices = {w: i for i, w in enumerate(words) if w in set(query)}
+    emscan = get_emscan_params(args, words, vectors, qindices)
+    emscan()
+
+
+if __name__ == '__main__':
+    main()
