7aa65c7a0ca536d0ac3f44cf190345c3272aec23
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2017-07-11 17:14:32 +0200

    Added remove_impotent().

--- python/scripts/draw_graph.py
+++ python/scripts/draw_graph.py
@@ -33,6 +33,14 @@ def parse_arguments():
     return args
 
 
+def remove_impotent(G):
+    """Removes all nodes from the initial iteration with no successors."""
+    to_delete = [node for node, data in G.nodes(data=True)
+                 if data['it'] == 0 and not list(nx.all_neighbors(G, node))]
+    print(to_delete)
+    G.remove_nodes_from(to_delete)
+
+
 def main():
     args = parse_arguments()
     logging.basicConfig(level=getattr(logging, args.log_level.upper()),
@@ -43,7 +51,7 @@ def main():
     colors = list(
         Color(args.from_color).range_to(Color(args.to_color), num_colors + 1))
     del colors[1]
-    print(colors)
+    remove_impotent(G)
     for node, data in G.nodes(data=True):
         G.node[node]['color'] = colors[data['it']].get_hex_l()
     A = nx.nx_agraph.to_agraph(G)
