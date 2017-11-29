790ab5fc81cbfd7f13f2f9fc008f08789a4cccc6
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2017-11-29 14:27:19 +0100

    Added FilterStopWords.

--- python/cc_emergency/functional/transforms/__init__.py
+++ python/cc_emergency/functional/transforms/__init__.py
@@ -10,6 +10,7 @@ from .dict_transforms import DeleteFields, RetainFields, FilterDictField
 from .dict_transforms import FilterDocument, FilterEmpty, NewField
 from .language_filter import LanguageFilter, DomainFilter
 from .minhash import MinHash
+from .nlp import FilterStopwords
 from .search import Search
 from .stats import WC
 
@@ -17,4 +18,4 @@ from .stats import WC
 __all__ = [CoreNLP, DeleteFields, RetainFields, FilterDictField,
            FilterDocument, FilterEmpty, NewField, LanguageFilter, DomainFilter,
            CoNLLToCounts, CoNLLToList, CoNLLToLists, CoNLLToSet,
-           CoNLLToText, MinHash, Search, WC]
+           CoNLLToText, MinHash, FilterStopwords, Search, WC]
