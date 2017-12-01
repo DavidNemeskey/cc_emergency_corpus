a1d2795da475fc76e6f5609b9fe6a8cbb271dd53
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2017-12-01 22:28:14 +0100

    Bigrams are now separated by space.

--- python/cc_emergency/functional/transforms/bigrams.py
+++ python/cc_emergency/functional/transforms/bigrams.py
@@ -13,7 +13,7 @@ class CreateBigrams(Map):
     """
     Creates bigram versions of selected fields in the document. In order to
     make it easier for text-based tools to handle these fields, the bigrams
-    are not tuples, but the two words joined by an underscore.
+    are not tuples, but the two words joined by a space.
 
     The name of the new field will be {field}_bigrams. If it already exists,
     it is left alone, unless the overwrite argument is True.
@@ -36,7 +36,7 @@ class CreateBigrams(Map):
             if field in obj:
                 if new_field not in obj or self.overwrite:
                     obj[new_field] = [
-                        '_'.join(bigram) for bigram in self.pairwise(obj[field])
+                        ' '.join(bigram) for bigram in self.pairwise(obj[field])
                         if all(map(self.filter.valid, bigram))
                     ]
         return obj
@@ -76,7 +76,7 @@ class BigramFilter(Map):
         Checks if a (_-joined bigram) has a valid split, i.e. both of its
         components are in the unigram set.
         """
-        for w1, w2 in self.all_splits(bigram):
+        for w1, w2 in bigram.split(' '):
             if w1 in self.s and w2 in self.s:
                 return True
         else:
@@ -85,6 +85,7 @@ class BigramFilter(Map):
     @staticmethod
     def all_splits(bigram):
         """Returns all possible splits of a(n underscore-joined) bigram."""
+        # DEPRECATED
         for m in BigramFilter.US_P.finditer(bigram):
             yield bigram[:m.span()[0]], bigram[m.span()[1]:]
 
@@ -117,7 +118,7 @@ class BigramFilter2(Map):
         Checks if a (_-joined bigram) has a valid split, i.e. both of its
         components are in the unigram set.
         """
-        for w1, w2 in self.all_splits(bigram):
+        for w1, w2 in bigram.split(' '):
             if w1 in unigrams and w2 in unigrams:
                 return True
         else:
@@ -126,5 +127,6 @@ class BigramFilter2(Map):
     @staticmethod
     def all_splits(bigram):
         """Returns all possible splits of a(n underscore-joined) bigram."""
+        # DEPRECATED
         for m in BigramFilter.US_P.finditer(bigram):
             yield bigram[:m.span()[0]], bigram[m.span()[1]:]
