c2768c4ebd3f9b74379cbc1cb3273e57786c5c6f
David Nemeskey <nemeskeyd@gmail.com>
2017-12-08 14:45:55 +0100

    Bugfixes + new features.

--- python/scripts/select_terms.py
+++ python/scripts/select_terms.py
@@ -20,12 +20,12 @@ def parse_arguments():
     parser.add_argument('--print', '-p', default='all',
                         choices=['new', 'query', 'missing', 'all'],
                         help='what to print when -q is specified. "new" '
-                             '(the default) prints terms not in the query '
+                             'prints terms not in the query '
                              'file; "query" is the opposite, while "missing" '
                              'prints the terms in that query file that are '
-                             'missing from the results. "all" prints '
-                             'everything, with the label as an additional '
-                             'column.')
+                             'missing from the results. The default, "all" '
+                             'prints everything, with the label as an '
+                             'additional column.')
     parser.add_argument('--sort-by', '-s', default='rdw',
                         help='what to sort the results by. "r" stands for '
                              'ratio, "d" for DF and "w" for the word. The '
@@ -37,17 +37,27 @@ def parse_arguments():
     parser.add_argument('--max-bdf', type=int, help='the maximum bigram df.')
     parser.add_argument('--min-ur', type=float, help='the minimum unigram ratio.')
     parser.add_argument('--min-br', type=float, help='the minimum bigram ratio.')
+    parser.add_argument('--min-udfr', type=int,
+                        help='the minimum unigram df ratio.')
+    parser.add_argument('--max-udfr', type=int,
+                        help='the maximum unigram df ratio.')
+    parser.add_argument('--min-bdfr', type=int,
+                        help='the minimum bigram df ratio.')
+    parser.add_argument('--max-bdfr', type=int,
+                        help='the maximum bigram df ratio.')
     args = parser.parse_args()
 
-    if not re.match('^[rdf]{1,3}$', args.sort_by):
+    if not re.match('^[rdw]{1,3}$', args.sort_by):
         parser.error('Invalid --sort-by pattern: only "r", "d", and "w" are '
                      'allowed, and each at most once.')
+    if args.print not in ['all', 'new'] and not args.query_file:
+        parser.error('-p cannot be set if -q isn\'t.')
     return args
 
 
 def read_input(input_file):
     with openall(input_file) as inf:
-        return [(w, float(r), *map(int, *_)) for w, r, *_ in
+        return [(w, float(r), *map(int, _)) for w, r, *_ in
                 (line.strip().split('\t') for line in inf)]
 
 
@@ -56,9 +66,10 @@ def sorter(pattern):
     indices = [{'w': 0, 'r': 1, 'd': 3}[c] for c in pattern]
     defaults = [{'w': None, 'r': 0, 'd': 0}[c] for c in pattern]
 
+    ig = itemgetter(*indices, defaults=defaults)
     def key(obj):
         return tuple(v if isinstance(v, str) else neg(v)
-                     for v in itemgetter(indices, defaults=defaults))
+                     for v in ig(obj))
     return key
 
 
@@ -67,7 +78,9 @@ def get_thresholds(args, unigram):
     return (
         getattr(args, 'min_{}r'.format(prefix)),
         getattr(args, 'min_{}df'.format(prefix)),
-        getattr(args, 'max_{}df'.format(prefix))
+        getattr(args, 'max_{}df'.format(prefix)),
+        getattr(args, 'min_{}dfr'.format(prefix)),
+        getattr(args, 'max_{}dfr'.format(prefix)),
     )
 
 
@@ -85,13 +98,18 @@ def main():
 
     sorted_data = {'new': [], 'query': []}
     for item in data:
-        min_r, min_df, max_df = get_thresholds(args, ' ' not in item[0])
+        min_r, min_df, max_df, min_dfr, max_dfr = get_thresholds(
+            args, ' ' not in item[0])
         if min_r is not None and float(item[1]) < min_r:
             continue
         if len(item) > 2:
-            if min_df is not None and int(item[-1]) < min_df:
+            if min_df is not None and item[-1] < min_df:
                 continue
-            if max_df is not None and max_df < int(item[-1]):
+            if max_df is not None and max_df < item[-1]:
+                continue
+            if min_dfr is not None and float(item[-1]) / item[-2] < min_dfr:
+                continue
+            if max_dfr is not None and max_dfr < float(item[-1]) / item[-2]:
                 continue
         if queries and item[0] in queries:
             sorted_data['query'].append(item)
@@ -101,15 +119,22 @@ def main():
         have_it = set(map(itemgetter(0), queries))
         sorted_data['missing'] = queries - have_it
 
-    if args.print == 'all':
-        for item in sorted(sorted_data['new'], key=sorter(args.sort_by)):
-            print('\t'.join(map(str, item)) + '\tnew')
-        for item in sorted(sorted_data['query'], key=sorter(args.sort_by)):
-            print('\t'.join(map(str, item)) + '\tquery')
-        for item in sorted(sorted_data['missing'], key=sorter(args.sort_by)):
-            print(item + '\tmissing')
+    if queries:
+        if args.print == 'all':
+            for item in sorted(sorted_data['new'], key=sorter(args.sort_by)):
+                print('\t'.join(map(str, item)) + '\tnew')
+            for item in sorted(sorted_data['query'], key=sorter(args.sort_by)):
+                print('\t'.join(map(str, item)) + '\tquery')
+            for item in sorted(sorted_data['missing']):
+                print(item + '\tmissing')
+        elif args.print == 'missing':
+            for item in sorted(sorted_data['missing']):
+                print(item)
+        else:
+            for item in sorted(sorted_data[args.print], key=sorter(args.sort_by)):
+                print('\t'.join(map(str, item)))
     else:
-        for item in sorted(sorted_data[args.print], key=sorter(args.sort_by)):
+        for item in sorted(sorted_data['new'], key=sorter(args.sort_by)):
             print('\t'.join(map(str, item)))
 
 if __name__ == '__main__':
