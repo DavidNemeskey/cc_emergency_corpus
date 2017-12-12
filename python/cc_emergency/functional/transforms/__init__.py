69e8fef22219d6d6183cacdfedc143307f0dc74c
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2017-12-12 10:20:57 +0100

    Added GetField.

--- python/cc_emergency/functional/transforms/__init__.py
+++ python/cc_emergency/functional/transforms/__init__.py
@@ -7,7 +7,7 @@ from .conll_converter import CoNLLToCounts, CoNLLToList, CoNLLToLists
 from .conll_converter import CoNLLToSet, CoNLLToText
 from .corenlp import CoreNLP
 from .dict_transforms import DeleteFields, RetainFields, FilterDictField
-from .dict_transforms import FilterDocument, FilterEmpty, NewField
+from .dict_transforms import FilterDocument, FilterEmpty, NewField, GetField
 from .language_filter import LanguageFilter, DomainFilter
 from .minhash import MinHash
 from .nlp import FilterStopwords
@@ -16,6 +16,7 @@ from .stats import WC
 
 
 __all__ = [CoreNLP, DeleteFields, RetainFields, FilterDictField,
-           FilterDocument, FilterEmpty, NewField, LanguageFilter, DomainFilter,
+           FilterDocument, FilterEmpty, NewField, GetField,
+           LanguageFilter, DomainFilter,
            CoNLLToCounts, CoNLLToList, CoNLLToLists, CoNLLToSet,
            CoNLLToText, MinHash, FilterStopwords, Search, WC]
