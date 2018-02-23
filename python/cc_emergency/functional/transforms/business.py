fb65863947fb8bcdafb2648a061f0e102a736439
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2018-02-23 11:03:37 +0100

    EntityExtractor done.

--- python/cc_emergency/functional/transforms/business.py
+++ python/cc_emergency/functional/transforms/business.py
@@ -10,21 +10,6 @@ from cc_emergency.functional.core import Map
 from cc_emergency.utils import openall
 
 
-class Extractor(Map):
-    """
-    Extracts documents from a collection that contain at least a certain
-    number and type of the keywords provided.
-    """
-    def __init__(self, keyword_file, min_keywords):
-        super(Extractor, self).__init__()
-        self.keywords = self.read_kw_file(keyword_file)
-        self.min_keywords = min_keywords
-
-    def read_kw_file(self, keyword_file):
-        with openall(keyword_file) as inf:
-            return set(tuple(line.strip().lower().split('\t')) for line in inf)
-
-
 class GetEntitiesFromConll(object):
     """
     Gets entities (word bursts whose kth CoNLL field matches a regex) from
@@ -52,18 +37,28 @@ class GetEntitiesFromConll(object):
         return entities
 
 
-class ExtractFromNews(Extractor):
-    """Extracts the documents from the news collections."""
-    def __init__(self, keyword_file, min_keywords, conll_field):
-        super(ExtractFromNews, self).__init__(keyword_file, min_keywords)
+class EntityExtractor(Map):
+    """
+    Extracts documents from a collection that contain at least a certain
+    number of entites.
+    """
+    def __init__(self, keyword_file, min_keywords, conll_field,
+                 word_field, type_field, type_regex):
+        super(EntityExtractor, self).__init__()
+        self.keywords = self.read_kw_file(keyword_file)
+        self.min_keywords = min_keywords
         self.conll_field = conll_field
+        self.ner = GetEntitiesFromConll(word_field, type_regex, type_regex)
+
+    def read_kw_file(self, keyword_file):
+        with openall(keyword_file) as inf:
+            return set(tuple(line.strip().lower().split()) for line in inf)
 
     def transform(self, obj):
         conll = obj.get(self.conll_field)
         if conll:
-            doc = set((token[0].lower(), token[2].lower())
-                      for sentence in conll for token in sentence)
-            intersection = len(doc & self.keywords)
+            entities = self.ner.get_entities(conll)
+            intersection = len(entities & self.keywords)
             if intersection >= self.min_keywords:
                 obj[self.conll_field + '_match'] = intersection
                 return obj
